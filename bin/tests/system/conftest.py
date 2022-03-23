518a2be13dc1822073702ed3f386efada70c1bcb
Michał Kępień <michal@isc.org>
2022-03-23 14:48:37 +0100

    Implement Python helpers for using RNDC in tests
    Controlling named instances using RNDC is a common action in BIND 9
    system tests.  However, there is currently no standardized way of doing
    that from Python-based system tests, which leads to code duplication.
    Add a set of Python classes and pytest fixtures which intend to simplify
    and standardize use of RNDC in Python-based system tests.
    
    For now, RNDC commands are sent to servers by invoking the rndc binary.
    However, a switch to a native Python module able to send RNDC commands
    without executing external binaries is expected to happen soon.  Even
    when that happens, though, having the capability to invoke the rndc
    binary (in order to test it) will remain useful.  Define a common Python
    interface that such "RNDC executors" should implement (RNDCExecutor), in
    order to make switching between them convenient.

--- bin/tests/system/conftest.py
+++ bin/tests/system/conftest.py
@@ -11,9 +11,14 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
+# pylint: disable=redefined-outer-name
+
+import logging
 import os
 
 import pytest
+
+import isctest
 
 
 @pytest.fixture(scope='session')
@@ -29,3 +34,32 @@ def named_tlsport():
 @pytest.fixture(scope='session')
 def control_port():
     return int(os.environ.get('CONTROLPORT', default=9953))
+
+
+@pytest.fixture(scope='module')
+def rndc_logger():
+    formatter = logging.Formatter('[%(asctime)s] %(message)s')
+    handler = logging.FileHandler('rndc.log')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger('rndc')
+    logger.addHandler(handler)
+    logger.setLevel('DEBUG')
+    return logger
+
+
+@pytest.fixture(scope='module')
+def servers(named_port, control_port, rndc_logger):
+    instances = {}
+    with os.scandir() as iterator:
+        for entry in iterator:
+            if entry.is_dir():
+                try:
+                    dir_name = entry.name
+                    ports = isctest.NamedPorts(dns=named_port,
+                                               rndc=control_port)
+                    instance = isctest.NamedInstance(dir_name, ports,
+                                                     rndc_logger)
+                    instances[dir_name] = instance
+                except ValueError:
+                    continue
+    return instances
