097627c978e3196b956e0e99c0122ee75c4dbaf7
Matthijs Mekking <matthijs@isc.org>
2022-12-05 10:21:38 +0100

    Remove unused dns_remote_t functions
    Now that setting alternate transfer sources is removed, the functions
    to check whether all addresses are considered good have become obsolete.

--- lib/dns/remote.c
+++ lib/dns/remote.c
@@ -409,35 +409,6 @@ dns_remote_done(dns_remote_t *remote) {
 	return (remote->curraddr >= remote->addrcnt);
 }
 
-bool
-dns_remote_allgood(dns_remote_t *remote) {
-	REQUIRE(remote != NULL);
-
-	if (remote->ok == NULL) {
-		return (true);
-	}
-
-	for (unsigned int i = 0; i < remote->addrcnt; i++) {
-		if (!remote->ok[i]) {
-			return (false);
-		}
-	}
-
-	return (true);
-}
-
-bool
-dns_remote_addrok(dns_remote_t *remote) {
-	REQUIRE(remote != NULL);
-	REQUIRE(remote->curraddr < remote->addrcnt);
-
-	if (remote->ok == NULL) {
-		return (true);
-	}
-
-	return (remote->ok[remote->curraddr]);
-}
-
 void
 dns_remote_mark(dns_remote_t *remote, bool good) {
 	REQUIRE(remote != NULL);
